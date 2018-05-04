83f57951b7013c5d7ba417a4236931400cfc19c5
sdelamo <sergio.delamo@softamo.com>
2018-05-04 08:47:33 +0200

    Initial repo

---
+++ complete/src/test/groovy/demo/BasicAuthSpec.groovy
@@ -0,0 +1,42 @@
+package demo
+
+import demo.services.AuthenticationProviderUserPassword
+import io.micronaut.context.ApplicationContext
+import io.micronaut.http.HttpResponse
+import io.micronaut.http.HttpStatus
+import io.micronaut.http.client.exceptions.HttpClientResponseException
+import io.micronaut.runtime.server.EmbeddedServer
+import spock.lang.AutoCleanup
+import spock.lang.Shared
+import spock.lang.Specification
+import io.micronaut.http.client.RxHttpClient
+import io.micronaut.http.HttpRequest
+
+class BasicAuthSpec extends Specification {
+
+    @Shared
+    @AutoCleanup // <1>
+    EmbeddedServer embeddedServer = ApplicationContext.run(EmbeddedServer) // <2>
+
+    @Shared
+    @AutoCleanup
+    RxHttpClient client = embeddedServer.applicationContext.createBean(RxHttpClient, embeddedServer.getURL()) // <3>
+
+    def "verify http basic auth works"() {
+        when: 'Accessing a secured URL without authenticating'
+        client.toBlocking().exchange(HttpRequest.GET('/', )) // <6>
+
+        then: 'returns unauthorized'
+        HttpClientResponseException e = thrown(HttpClientResponseException)  // <7>
+        e.status == HttpStatus.UNAUTHORIZED
+
+        when: 'A secured URL, is accessed with Basic Auth'
+        HttpRequest request = HttpRequest.GET('/', )
+                .basicAuth("sherlock", "password") // <8>
+        HttpResponse<String> rsp = client.toBlocking().exchange(request, String) // <9>
+
+        then: 'the endpoint can be accessed'
+        rsp.status == HttpStatus.OK
+        rsp.body() == 'sherlock' // <10>
+    }
+}
